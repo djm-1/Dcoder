1d6f38fd7238e8faeb45f026abac12c6cb582f67
Dibyajyoti Mondal <dibyajyoti.bhs@gmail.com>
2022-08-19 12:43:18 +0530

    initial commit

--- client/src/Editor.js
+++ client/src/Editor.js
@@ -75,7 +75,7 @@ export default function Editor() {
 
   
   useEffect(() => {
-    const s = io(`/:${process.env.PORT}`);
+    const s = io(`/:${process.env.PORT}/`);
     setsocket(s);
     //when I join a room
     s.emit("join", {
